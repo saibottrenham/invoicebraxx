3581218652b2387aa208c7e748e3fd1886458b93
Tobias Mahnert <tobiasmahnert@Tobiass-MacBook-Pro.local>
2018-06-10 19:47:49 +1000

    best practice initial monolithic commit, fight me soyboys

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # invoicebraxx
 Nifty little invoicing tool to speed up my Sole trader invoicing process
+
+## Set up instructions
+
+install requirements with 
+
+	pip install -r req.txt
+
+Google Slides API instructions
+https://developers.google.com/slides/quickstart/python
+
+NOTE:
+Use a service account so that you don't have to go through the click authentication process. 
+
+create your own Invoice template with Google Slides inside your personal google drive and 
+use the service account email to give access to the service account.
+
+check the PDF as an example.
+
+For the Emailing process I used https://beefree.io/templates/
+to create a Basic html thank you Email with links to my Linkedin
+
+inorder to get the images appear in your email, you have to replace the /images/...png urls with 
+cid:Content-IDs
+
+eg:
+
+	/images/clouds_outline.png
+	
+becomes:
+
+	cid:clouds_outlines
+
+## Config
+The ongoing invoice number can be set inside invoiceNOExample.csv
+the increment rate can be edited on line 29 inside invoiceYou.py
+
+The Date is set for the day of invoice generation.
+The Due Date is creation date plus 7 days.
+
+It can be changed on line 16 inside invoiceYou.py
+
+Bank contacts are stored inside 
+bankContactsExample.csv
+
+Clients are stored inside clientsExample.csv and can be selected via client nickname 
+
+The same schema for personal data which is inside mydataExample.csv
+
+Remember to enable your SMTP service if you want it to use with email functionality
+
+Note:
+if you want to proof read your Invoice before sending them out to a client, you can easily sent them 
+to yourself and then forward them
+
+
+## how to run
+
+	python invoiceYou.py myNickName cleintNickName  rate qty item
+
+Example:
+
+	python invoiceMe.py 'toby' 'mrOne' 150 10 'Webdevelopment and consulting'
+
+
+## todo and next steps
+
+further development includes
+* create multi line Item functionality
+* set up tax options
+* cleaning up, refactoring this big monolithic clusterf***
+* implementing inside a webapplication, possibly django
+* connect to an accounting system
---
+++ invoiceYou.py
@@ -0,0 +1,210 @@
+"""
+Invocing script
+Author: Tobias Mahnert
+Date: 10.06.2018
+"""
+from __future__ import print_function
+from apiclient import discovery
+import httplib2
+from oauth2client.service_account import ServiceAccountCredentials as SAC
+import pandas as pd
+import datetime
+import sys
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEImage import MIMEImage
+from email.mime.application import MIMEApplication
+import codecs
+import glob
+import smtplib
+import os
+
+
+def createInvoiceFolders(date):
+	if not os.path.exists(date):
+		os.makedirs(date)
+
+
+def appendReq(key, value):
+	reqs.append({'replaceAllText': {'containsText': {'text': "{{"+key+"}}"},
+                            'replaceText': str(value)
+                            }})
+	return
+
+
+def getTheInfo(csv, col, nickName):
+	df = pd.read_csv(csv)
+	info = df.loc[df[col] == nickName].to_dict(orient='records')
+	for key, value in info[0].iteritems():
+		if key in varsDict:
+			varsDict[key] = value
+		appendReq(key, value)
+	return	
+
+
+def attachEmailImages(image, imgref):
+	fp = open(image, 'rb')
+	msgImage = MIMEImage(fp.read())
+	fp.close()
+	# Define the image's ID as referenced above
+	msgImage.add_header('Content-ID', '<'+imgref+'>')
+	msgRoot.attach(msgImage)
+
+
+start_date = datetime.datetime.today()
+end_date = start_date + datetime.timedelta(days=7)
+
+
+# load the UserInput
+reqs = []
+myNickName = sys.argv[1]
+clientNickname = sys.argv[2]
+rate = float(sys.argv[3])
+qty = float(sys.argv[4])
+item = sys.argv[5]
+
+
+# get latest invoive NO and append a new one
+invoiceDF = pd.read_csv('exampleCSV/invoiceNOExample.csv')
+latestNO = invoiceDF['invoiceNO'].tail(1)
+latestInvoice = latestNO[len(invoiceDF)-1]
+newInvoice = latestInvoice + 10
+df2 = pd.DataFrame([newInvoice], columns=list(['invoiceNO']))
+invoiceDF = invoiceDF.append(df2)
+
+
+month = start_date.strftime('%m')
+year = start_date.strftime('%Y')
+
+
+varsDict = {'clientName' : '',
+			'myName' : '',
+			'myEmail' : '',
+			'clientEmail' : '',
+			'preferredTemplate' : '',
+			'host' : '',
+			'port' : '',
+			'username' : '',
+			'password' : ''}
+
+
+createInvoiceFolders('invoices/' + year + '/' + month)
+
+
+# append the newInvoice No
+configDetails = {'invoiceNr': newInvoice, 
+				'date' : start_date.strftime('%Y-%m-%d'), 
+				'dueDate': end_date.strftime('%Y-%m-%d'),
+				'item': item,
+				'qty': qty,
+				'rate': rate,
+				'amount': "{0:.2f}".format(qty * rate),
+				'subtotal': str("{0:.2f}".format(qty * rate)) + '\n',
+				'total': "{0:.2f}".format(qty * rate),
+				'bDue' : "{0:.2f}".format(qty * rate)
+				 }
+
+
+details = [['exampleCSV/clientsExample.csv', 'cleintNickName', clientNickname],
+		   ['exampleCSV/mydataExample.csv', 'myNickName', myNickName],
+		   ['exampleCSV/bankContactsExample.csv', 'nickname', myNickName],
+		   ['exampleCSV/myEmailDataExample.csv', 'emailNickame', myNickName]]
+
+
+print('about to get all the info')
+# append misc Details
+for key, value in configDetails.iteritems():
+	appendReq(key, value)
+
+
+# get clientDetails and select by nickname
+for i in details:
+	getTheInfo(i[0], i[1], i[2])
+
+
+print('about to initiate connection to google')
+
+
+creds = SAC.from_json_keyfile_name('client_secret.json',
+                                   scopes=["https://www.googleapis.com/auth/drive",
+                                           "https://www.googleapis.com/auth/presentations"])
+
+
+# initialize google slides and Drive API connection
+http = creds.authorize(httplib2.Http())
+service = discovery.build('drive', 'v3', http=http)
+SLIDES = discovery.build('slides', 'v1', http=http)
+result = service.files().list().execute()
+file_list = result["files"]
+
+
+while "nextPageToken" in result:
+    result = self.get_service().files().list(pageToken=result["nextPageToken"]                                             ).execute()
+    file_list = file_list + result["files"]
+
+
+for i in file_list:
+    if i['name'] == "billingTemplate":
+        print('found the template')
+        file_id = i['id']
+
+
+DATA = {'name': 'Invoice ' + str(newInvoice) }
+DECK_ID = service.files().copy(body=DATA, fileId=file_id).execute().get('id')
+SLIDES.presentations().batchUpdate(body={'requests': reqs},
+                                   presentationId=DECK_ID).execute()
+
+
+data = service.files().export(fileId=DECK_ID,
+                              mimeType='application/pdf').execute()
+f = open("invoices/"+year+"/"+month+"/"+str(newInvoice)+".pdf", 'wb')
+f.write(data)
+f.close()
+
+# Send an HTML email with an embedded image and a plain text message for
+# email clients that don't want to display the HTML.
+
+# Define these once; use them twice!
+
+
+# Create the root message and fill in the from, to, and subject headers
+
+msgRoot = MIMEMultipart('mixed')
+
+subject = varsDict['myName']+"Invoice: #"+str(newInvoice)
+msgRoot['Subject'] = subject
+msgRoot['From'] = varsDict['myEmail']
+msgRoot['To'] = varsDict['clientEmail']
+msgRoot.preamble = 'This is a multi-part message in MIME format.'
+fp = open("invoices/"+year+"/"+month+"/"+str(newInvoice)+".pdf", 'rb')
+
+attach = MIMEApplication(fp.read(), 'pdf')
+fp.close()
+attach.add_header('Content-Disposition', 'attachment', filename = 'Invoice_'+str(newInvoice)+'.pdf')
+msgRoot.attach(attach)
+
+
+# We reference the image in the IMG SRC attribute by the ID we give it below
+f=codecs.open("emailTemplates/" + varsDict['preferredTemplate'] +'.html' , 'r')
+
+msgText = MIMEText(f.read().replace('{{client}}', varsDict['clientName']), 'html')
+msgRoot.attach(msgText)
+
+# This example assumes the image is in the current directory
+for filename in glob.glob('emailTemplates/images/'+varsDict['preferredTemplate']+'/*'):
+	filesNames = filename.split('/')
+	contentID = filesNames[-1].split('.')
+	attachEmailImages(filename, contentID[0])
+
+# Send the email (this example assumes SMTP authentication is required)
+# print(varsDict)
+smtp = smtplib.SMTP(varsDict['host'], varsDict['port'])
+smtp.starttls()
+smtp.login(varsDict['username'], varsDict['password'])
+smtp.sendmail(varsDict['myEmail'], varsDict['clientEmail'], msgRoot.as_string())
+smtp.quit()
+
+# save the new invoice number to list of invoices
+invoiceDF.to_csv('exampleCSV/invoiceNOExample.csv', index=False)
+
+print('finished')
